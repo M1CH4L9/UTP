7f16f217a2b5d0593c709d5f20d1adf7402f02b9
Michał <78867292+M1CH4L9@users.noreply.github.com>
2025-12-01 18:44:34 +0100

    Add files via upload

--- UTP6_BM_S33975/UTP6_BM_S33975/src/zad1/Main.java
+++ UTP6_BM_S33975/UTP6_BM_S33975/src/zad1/Main.java
@@ -1,6 +1,6 @@
 /**
  *
- *  @author Berlak Michał S33975
+ * @author Berlak Michał S33975
  *
  */
 
@@ -11,37 +11,32 @@ import java.util.stream.*;
 import java.io.*;
 import java.net.*;
 
-
 public class Main {
-  public static void main(String[] args) {
-      try{
-          //URL url = new URL("http://wiki.puzzlers.org/pub/wordlists/unixdict.txt");
-          URL url = java.net.URI.create("https://web.archive.org/web/20180820022359if_/http://wiki.puzzlers.org/pub/wordlists/unixdict.txt").toURL();
-          BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://web.archive.org/web/20180820022359if_/http://wiki.puzzlers.org/pub/wordlists/unixdict.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 
-          Map<String, List<String>> anagramy = reader.lines().collect(Collectors.groupingBy(slowo -> {
-              char[] znaki = slowo.toCharArray();
-              Arrays.sort(znaki);
-              return new String(znaki);
-          }));
+                Map<String, List<String>> anagramy = reader.lines().collect(Collectors.groupingBy(slowo -> {
+                    char[] znaki = slowo.toCharArray();
+                    Arrays.sort(znaki);
+                    return new String(znaki);
+                }));
 
-          int maxLiczba = anagramy.values().stream().mapToInt(lista -> lista.size()).max().orElse(0);
+                int maxLiczba = anagramy.values().stream().mapToInt(lista -> lista.size()).max().orElse(0);
 
-          anagramy.values().stream()
-                  .filter(lista -> lista.size() == maxLiczba)
-                  .forEach(lista -> {
-                      String wynik = "";
-                      for(String s : lista){
-                          wynik = wynik + s + " ";
-                      }
-                      System.out.println(wynik.trim());
-                  });
-      }catch (Exception e){
-          e.printStackTrace();
-      }
-  }
+                anagramy.values().stream()
+                        .filter(lista -> lista.size() == maxLiczba)
+                        .forEach(lista -> {
+                            String wynik = "";
+                            for(String s : lista){
+                                wynik = wynik + s + " ";
+                            }
+                            System.out.println(wynik.trim());
+                        });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-
-
-
